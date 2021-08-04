1233c778afb8b18f912883de9620e4f4c899c034
Sreyom31 <sreyom31@gmail.com>
2021-08-04 08:30:58 +0530

    add the create, delete note feature.

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,13 +1,17 @@
 import React from "react";
-import { isPropertySignature } from "typescript";
 
 function Note(props) {
-    return (
+  function handleClick() {
+    props.onDelete(props.id);
+  }
+
+  return (
     <div className="note">
-        <h1>{props.title}</h1>
-        <p>{props.content}</p>
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
+      <button onClick={handleClick}>DELETE</button>
     </div>
-    )
+  );
 }
 
 export default Note;
